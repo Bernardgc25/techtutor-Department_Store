9a31979ade83c0144694b1d7fab509485f6ca3de
Bernard GoleCruz <golecruz@unlv.nevada.edu>
2023-06-18 13:39:16 -0700

    restructure menu

--- src/main/java/com/example/demo/OptionMenu/optionMenu.java
+++ src/main/java/com/example/demo/OptionMenu/optionMenu.java
@@ -11,11 +11,64 @@ public class optionMenu {
 
     Scanner scanner = new Scanner(System.in);
 
+    //console menu
+    public int adminMenu(int opt){
+        //String[] options;
 
-    public char adminMenu(char choice){
         System.out.print("\033[H\033[2J");  
 		System.out.flush();
 
+        System.out.print("Choose your option : ");
+
+        String[] options = {"1- Search a product by productID",
+                            "2- Search a product by productName",
+                            "3- Display all products",
+                            "4- Display products by Category",
+                            "5- Display total Expenses",
+                            "6- Display total Profits",
+                            "7- Exit",
+                            
+                        };  
+
+        int option = 1;
+        while (option!=4){
+            printMenu(options);
+            try {
+                option = scanner.nextInt();
+                switch (option){
+                    case 1: 
+                        break;
+                    
+                    case 2:  
+                        break;
+                    
+                    case 3: 
+                        break;
+                    
+                    case 4: 
+                        break;
+                    
+                    case 5: 
+                        break;
+                    
+                    case 6: 
+                        break;                    
+                    
+                    case 7: 
+                        System.out.print("\033[H\033[2J");  
+		                System.out.flush();
+                        break;
+                }
+            }
+            catch (Exception ex){
+                System.out.println("Please enter an integer value between 1 and " + options.length);
+                scanner.next();
+            }
+        }
+
+        return opt;
+
+        /* 
         System.out.println("You are logged in as Admin..");
         System.out.println("(I) Search a product by productID");
         System.out.println("(N) Search a product by productName");
@@ -97,7 +150,8 @@ public class optionMenu {
         
         
         return choice;
-
+        */
+    
     }
 
     public char customerMenu(char choice){
@@ -105,4 +159,11 @@ public class optionMenu {
 
     }
 
-}
+        private void printMenu(String[] options) {
+    }
+
+
+
+    }
+
+
