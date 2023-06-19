947b11a6ec636fa84abbddd84d45cefafeff66f7
Bernard GoleCruz <golecruz@unlv.nevada.edu>
2023-06-18 21:46:52 -0700

    done: optionMenu Exception

--- src/main/java/com/example/demo/OptionMenu/optionMenu.java
+++ src/main/java/com/example/demo/OptionMenu/optionMenu.java
@@ -1,5 +1,6 @@
 package com.example.demo.OptionMenu;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 import com.example.demo.DAOclass.StoreInformationDAOimplement;
@@ -16,51 +17,75 @@ public class optionMenu {
 
     //console menu
     public int adminMenu(int opt){
-
-        //clear screen
-        System.out.print("\033[H\033[2J");  
-		System.out.flush();
-
-        //System.out.println("Choose your option: ");
-        System.out.println("Menu");
-        System.out.println("0 - Exit");
-        System.out.println("1 - Search a product by productID");
-        System.out.println("2 - Search a product by productName");  
-        System.out.println("3 - Display all products");
-        System.out.println("4 - Display products by Category");
-        System.out.println("5 - Display total Expenses");
-        System.out.println("6 - Display total Profits");
+        boolean notDone = true;
         
-        
-        System.out.println("");
-        System.out.print("Enter a choice: ");
-        opt = scanner.nextInt();
+        while(notDone){
+            //clear screen
+            System.out.print("\033[H\033[2J");  
+            System.out.flush();
 
-        switch (opt){
-            case 0: 
+            //System.out.println("Choose your option: ");
+            System.out.println("Option Menu");
+            System.out.println("0 - Exit");
+            System.out.println("1 - Search a product by productID");
+            System.out.println("2 - Search a product by productName");  
+            System.out.println("3 - Display all products");
+            System.out.println("4 - Display products by Category");
+            System.out.println("5 - Display total Expenses");
+            System.out.println("6 - Display total Profits");
+            
+            
+            System.out.println("");
+            System.out.print("Enter a choice: ");
+            try {
+                opt = scanner.nextInt();
+
+                switch (opt){
+                case 0: 
+                    System.out.print("\033[H\033[2J");  
+                    System.out.flush();
+                    notDone = false; 
+                    break; 
+                case 1: 
+                    sm.searchbyProductId();
+                    break;
+                case 2:  
+                    sm.searchbyProductName();
+                    break;
+                case 3:
+                    sm.dispsallProduct();
+                    break;
+                case 4:
+                    sm.dispProductbyCategory(); 
+                    break;
+                case 5:
+                    sm.dispExpenses(); 
+                    break;
+                case 6:
+                    sm.dispProfit(); 
+                    break;                    
+                }       
+            } 
+            catch (InputMismatchException e) {
+                //clear screen
                 System.out.print("\033[H\033[2J");  
                 System.out.flush();
-                break; 
-            case 1: 
-                sm.searchbyProductId();
-                break;
-            case 2:  
-                sm.searchbyProductName();
-                break;
-            case 3:
-                sm.dispsallProduct();
-                break;
-            case 4:
-                sm.dispProductbyCategory(); 
-                break;
-            case 5:
-                sm.dispExpenses(); 
-                break;
-            case 6:
-                sm.dispProfit(); 
-                break;                    
-            }       
+                
+                System.out.println("Invalid input, integer only");
+                System.out.println("press any character to continue");
+    
+                char choice = scanner.next().charAt(0);
 
+                choice = scanner.next().charAt(0);
+                if((choice == 'c') || (choice == 'C')){
+                    System.out.println("Back to Option Menu");
+                    notDone = true; 
+                }
+            }
+
+            
+        }
+        
         return opt;    
     }
 
@@ -70,7 +95,6 @@ public class optionMenu {
         System.out.print("\033[H\033[2J");  
 		System.out.flush();
 
-   
         return opt;
     }
 
